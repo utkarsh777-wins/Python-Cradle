412865d1b8c26795c34ee76feff93f49de3362da
utkarsh777-wins <utkarsh777wins@gmail.com>
2025-11-09 18:47:42 +0530

    some codetantra questions

---
+++ codeTQ.py
@@ -0,0 +1,24 @@
+# olleh
+def reverse_string(s):
+    return s[::-1]
+print(reverse_string('hello'))
+
+# 1
+def tuple_function():
+    return (1, 2, 3)
+result = tuple_function()
+print(result[0])
+
+# 5.0
+def divide(a, b):
+    return a / b
+print(divide(10, 2))
+
+# [1, 2, 3, 4]
+def modify_list(lst):
+    lst.append(4)
+    return(lst)
+
+my_list = [1, 2 ,3]
+print(modify_list(my_list))
+
---
+++ codeTQ1.py
@@ -0,0 +1,12 @@
+txt = "Python"
+print(txt.endswith('n'))    #True
+
+print(max("hello world"))
+# w
+
+a = "{:.2f}".format(3.14159)
+print(a)   # 3.14
+
+txt = "123abc"
+print(txt.isdigit())    #False
+
